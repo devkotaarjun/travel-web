22739c0e397ee3dc134cc60e8595aa095c163862
devkotaarjun <devarjung@gmail.com>
2024-09-30 13:19:40 +0545

    first push to git

---
+++ exte.js
@@ -0,0 +1,62 @@
+
+
+alert("Welcome To My Website");
+
+
+confirm("Do you want to continue?");
+
+var user_name=prompt("Enter Your Name");
+alert("Your name is"+user_name)
+
+//== is equal to
+//=== is exactly to
+// !=it Not equalt to
+//<less then
+//greater then
+//<=less than or ewual to
+//greater then or equal to
+
+let numb=100;
+if(numb==100){
+    alert("Hello");
+}
+
+/*let number=102;
+if(number % 2 ==0){
+    alert(number+"is an even number");
+} else if(number% 3 ==0){
+    alert(number+"is a number divisle by 3");
+
+} else if( number%5==0){
+    alert(number+"is a number divisle by 5");
+
+} else{
+    alert(number+"is an unknown number");
+ 
+}*/
+
+/*let number =prompt("Enter a number");
+if (numb % 2 == 0){
+    alert(number+"is an even number");   
+} else{
+    alert(number+"is an odd number");
+}*/
+  //yoooooooooooo
+
+/*var height =prompt(" Plese enter your hight(m)");
+var weight =prompt(" Plese enter your weight(kg)");
+
+var BMI =weight / (height*height);
+
+var result;
+if (BMI < 18.5) {
+    result = "underweight";
+} else if(BMI >=18.5 && BMI < 25){
+  result = "normal weight";
+} else if(BMI >=25 && BMI < 30){
+  result = "over weight"; 
+} else if(BMI >=30){
+    result = "obese";
+}
+alert("Your BMI is" + BMI + ".You are" +result);*/
+
